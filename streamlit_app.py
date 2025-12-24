b2d93956f403eab2fbf5a177d92c89d7a9134faa
peteburnettvisuals <petercameronburnett@gmail.com>
2025-12-24 16:00:11 +0000

    R3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
-import streamlit as st
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
 
-st.title("🎈 Streamlit Test App")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit as st
+import pandas as pd
+
+st.write("Here's our first attempt at using data to create a table:")
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
