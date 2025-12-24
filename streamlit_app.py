e810cd084b27f62e68ef25b43538aa33d679959c
peteburnettvisuals <petercameronburnett@gmail.com>
2025-12-24 18:13:21 +0000

    r9

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
-import pandas as pd
 
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    })
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
 
-option = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
-
-'You selected: ', option
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
