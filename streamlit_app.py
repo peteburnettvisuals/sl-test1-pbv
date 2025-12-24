a5708d8520057ec4fba8dae01e2a44501590fd99
peteburnettvisuals <petercameronburnett@gmail.com>
2025-12-24 15:41:17 +0000

    R2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Streamlit")
+st.title("🎈 Streamlit Test App")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
